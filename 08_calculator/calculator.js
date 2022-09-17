5e76429bbbeed437caa4f76a2245971d7d365763
Nate Shuster <shusternathaniel@gmail.com>
2022-09-17 14:59:55 -0400

    complete calculator problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const final = array.reduce((partialSum, a) => partialSum + a, 0);
+  return final;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const product = array.reduce((a, b) => a * b, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	const final = Math.pow(a, b);
+  return final;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  let ans = 1
+  for (let i = 1; i <= num; i++) {
+    ans = i*ans
+  }
+
+  return ans;
 };
 
 // Do not edit below this line
